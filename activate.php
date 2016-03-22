a76f725e984ff954494d2f680853031b6e9f450a
Viet Nguyen <viet@users.noreply.github.com>
2016-03-23 00:26:17 +0700

    Update activate.php

--- activate.php
+++ activate.php
@@ -1,5 +1,5 @@
 <pre>
 <?php
 require_once "RailAPI.php";
-$api = new RailAPI("YOUR_CLOUDFLARE_PARTNER_API");
+$api = new RailAPI("YOUR_CLOUDFLARE_PARTNER_API_KEY");
 var_dump($api->set("YOUR_DOMAIN_NAME", "YOUR_DOMAIN_KEY"));
